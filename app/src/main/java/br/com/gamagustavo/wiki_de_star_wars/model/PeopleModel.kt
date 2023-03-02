9a0717d2017c9d153bc2ee4ba164068dff2d88a6
GamaGustavo <gustavo98ag@gmail.com>
2023-03-02 12:15:51 -0300

    feat: Crição da data class PeopleModel.
    Corresponde ao resource People da api SWAPI.
    A classe PeopleModel contém apenas os atributos exigidos pelo documento do teste.

---
+++ app/src/main/java/br/com/gamagustavo/wiki_de_star_wars/model/PeopleModel.kt
@@ -0,0 +1,19 @@
+package br.com.gamagustavo.wiki_de_star_wars.model
+
+import com.google.gson.annotations.SerializedName
+
+data class PeopleModel(
+    val name: String,
+    val height: String,
+    val mass: String,
+    @SerializedName(value = "hair_color") val hairColor: String,
+    @SerializedName(value = "skin_color") val skinColor: String,
+    @SerializedName(value = "eye_color") val eyeColor: String,
+    @SerializedName(value = "birth_year") val birthYear: String,
+    val gender: String,
+    val homeworld: String,
+    val species: List<String>,
+    val url: String,
+    val created: String,
+    val edited: String
+)
