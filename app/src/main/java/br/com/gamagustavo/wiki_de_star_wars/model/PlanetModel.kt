d587a9679727e97ca0669a7b68d1882b04a2bbeb
GamaGustavo <gustavo98ag@gmail.com>
2023-03-03 11:27:12 -0300

    feat: Criação dos models de Planet e Specie.

---
+++ app/src/main/java/br/com/gamagustavo/wiki_de_star_wars/model/PlanetModel.kt
@@ -0,0 +1,8 @@
+package br.com.gamagustavo.wiki_de_star_wars.model
+
+data class PlanetModel(
+    val name: String,
+    val url: String,
+    val created: String,
+    val edited: String,
+)
---
+++ app/src/main/java/br/com/gamagustavo/wiki_de_star_wars/model/SpecieModel.kt
@@ -0,0 +1,8 @@
+package br.com.gamagustavo.wiki_de_star_wars.model
+
+data class SpecieModel(
+    val name: String,
+    val url: String,
+    val created: String,
+    val edited: String,
+)
