4c04270668907242b388983332084ddce3be9452
GamaGustavo <gustavo98ag@gmail.com>
2023-03-03 11:45:53 -0300

    refact: remoção do método getPeopleById.
    Metodo nuca foi e nem será utilizado.

--- app/src/main/java/br/com/gamagustavo/wiki_de_star_wars/http/PeopleWebClient.kt
+++ app/src/main/java/br/com/gamagustavo/wiki_de_star_wars/http/PeopleWebClient.kt
@@ -1,7 +1,6 @@
 package br.com.gamagustavo.wiki_de_star_wars.http
 
 import br.com.gamagustavo.wiki_de_star_wars.model.ListPeopleModel
-import br.com.gamagustavo.wiki_de_star_wars.model.PeopleModel
 import com.google.gson.Gson
 
 object PeopleWebClient {
@@ -24,11 +23,5 @@ object PeopleWebClient {
         parameters.add(Pair("search", name))
         val json = WebClient.get(PATH, parameters)
         return GSON.fromJson(json, ListPeopleModel::class.java)
-    }
-
-    suspend fun getPeopleById(id: Int): PeopleModel {
-        val pathWithId = "$PATH/$id/"
-        val json = WebClient.get(pathWithId)
-        return GSON.fromJson(json, PeopleModel::class.java)
     }
 }
