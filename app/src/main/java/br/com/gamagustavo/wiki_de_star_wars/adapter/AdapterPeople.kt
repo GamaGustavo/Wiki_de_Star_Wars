8899915c3fcee86b223d48cb1e68671bb480fbcb
GamaGustavo <gustavo98ag@gmail.com>
2023-03-04 16:34:07 -0300

    feat: Implementação do onclik e preenchimento de novos Atributos.
    Foar implementado o método onClik dos itens de people presentes na recicle view.
    
    Fora adicionado os restantes atributos de PeopleModel nos itens.

--- app/src/main/java/br/com/gamagustavo/wiki_de_star_wars/adapter/AdapterPeople.kt
+++ app/src/main/java/br/com/gamagustavo/wiki_de_star_wars/adapter/AdapterPeople.kt
@@ -4,12 +4,17 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.gamagustavo.wiki_de_star_wars.R
 import br.com.gamagustavo.wiki_de_star_wars.model.PeopleModel
 
-class AdapterPeople(private val context: Context, private val peoples: MutableList<PeopleModel>) :
+class AdapterPeople(
+    private val context: Context,
+    private val peoples: MutableList<PeopleModel>
+) :
     RecyclerView.Adapter<AdapterPeople.PeopleViewHolder>() {
 
     inner class PeopleViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
@@ -17,6 +22,14 @@ class AdapterPeople(private val context: Context, private val peoples: MutableLi
         val mass: TextView = itemView.findViewById(R.id.vl_mass)
         val height: TextView = itemView.findViewById(R.id.vl_height)
         val gender: TextView = itemView.findViewById(R.id.vl_gender)
+        val card: CardView = itemView.findViewById(R.id.cv_item)
+        val hairColor: TextView = itemView.findViewById(R.id.vl_hair_color)
+        val skinColor: TextView = itemView.findViewById(R.id.vl_skin_color)
+        val eyeColor: TextView = itemView.findViewById(R.id.vl_eye_color)
+        val birthYear: TextView = itemView.findViewById(R.id.vl_birth_year)
+        val homeworld: TextView = itemView.findViewById(R.id.vl_homeworld)
+        val species: TextView = itemView.findViewById(R.id.vl_specie)
+        val expandeIten: LinearLayout = itemView.findViewById(R.id.ln_expande_itens)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PeopleViewHolder {
@@ -27,20 +40,28 @@ class AdapterPeople(private val context: Context, private val peoples: MutableLi
     override fun getItemCount(): Int = peoples.size
 
     override fun onBindViewHolder(holder: PeopleViewHolder, position: Int) {
+        holder.card.setOnClickListener {
+            holder.expandeIten.visibility =
+                if (holder.expandeIten.visibility != View.VISIBLE) View.VISIBLE else View.GONE
+        }
         val people = peoples[position]
         holder.name.text = people.name
-        holder.mass.text =
-            if ("unknown" == people.mass || "n/a" == people.mass) {
-                people.mass
-            } else {
-                people.mass.plus(" kg")
-            }
-        holder.height.text =
-            if ("unknown" == people.height || "n/a" == people.height) {
-                people.height
-            } else {
-                people.height.plus(" cm")
-            }
+        holder.mass.text = insertUnit(people.mass, " Kg")
+        holder.height.text = insertUnit(people.height, " cm")
         holder.gender.text = people.gender
+        holder.birthYear.text = people.birthYear
+        holder.eyeColor.text = people.eyeColor
+        holder.homeworld.text = people.name
+        holder.hairColor.text = people.hairColor
+        holder.skinColor.text = people.skinColor
+        holder.species.text = people.name
     }
+
+    fun insertUnit(string: String, unit: String): String {
+        return if ("unknown" == string || "n/a" == string) string else string.plus(unit)
+    }
+}
+
+interface ClickPeopleIten {
+    fun clickPeopleIten(context: Context, peopleModel: PeopleModel)
 }
